f7a0ab8307ff46b49c936a89d4bcb5e6a73b305f
Pavelklm <pahanklm01@gmail.com>
2023-11-02 22:33:13 +0200

    fix

--- server.js
+++ server.js
@@ -1,29 +1,36 @@
-const express = require('express')
-const port = 8080
+const express = require('express');
+// const cheerio = require('cheerio'); // Закомментировать эту строку
+// const puppeteer = require('puppeteer'); // Закомментировать эту строку
+const port = 8080;
 
-const app = express()
+const app = express();
 
 // Добавьте заголовки CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://astron.co.ua')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
-    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', 'https://astron.co.ua');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    next()
-})
+  next();
+});
 
-app.use('/', (req, res) => {
-    try {
-			return {hello: 'world'}
-        // Ваш код без Puppeteer, например, можно заменить его на заглушку
-        // res.json({ data: "Здесь был код Puppeteer, но теперь его нет" });
-    } catch (error) {
-        console.error(error)
-        res.status(500).send('Ошибка при выполнении запроса')
-    }
-})
+app.use('/', async (req, res) => {
+  try {
+    // Весь код, связанный с Puppeteer, закомментировать
+    // ...
+
+    // Отправляем JSON с надписью "Привет"
+    res.json({ message: 'Привет' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Ошибка при выполнении запроса');
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Прокси-сервер запущен на порту ${port}`)
-})
+  console.log(`Прокси-сервер запущен на порту ${port}`);
+});
