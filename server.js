afa02d8ddde7b6c26b4edbeb5bcdca87c007d67f
Pavelklm <pahanklm01@gmail.com>
2023-11-03 12:30:43 +0200

    fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/', async (req, res) => {
 		const url = ''
 			'https://www.google.com/maps/place/"АСТРОН"+ПЕЧАТКИ+ШТАМПИ/@49.0609627,33.4066144,16z/data=!4m6!3m5!1s0x40d753261b91eb75:0x9e7824fc7654ecc1!8m2!3d49.0609409!4d33.4077034!16s%2Fg%2F1vc80b9l?hl=uk&entry=ttu&callback=processData'
 
-		const browser = await puppeteer.launch()
+		const browser = await puppeteer.launch({headless: false})
 		const page = await browser.newPage()
 		await page.goto(url, { waitUntil: 'domcontentloaded' })
 
@@ -80,7 +80,7 @@ app.use('/', async (req, res) => {
 		})
 	} catch (error) {
 		console.error(error)
-		res.status(500).send('lol')
+		// res.status(500).send('Ошибка при выполнении запроса')
 	}
 })
 
