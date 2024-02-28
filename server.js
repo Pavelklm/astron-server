3e697c81e2b6e1307b45a5f0a5ceddaaa9011fe3
Pavelklm <pahanklm01@gmail.com>
2024-02-29 00:58:35 +0200

    comment +

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 
 app.use((req, res, next) => {
   // res.setHeader('Access-Control-Allow-Origin', 'https://astron.co.ua');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-  // res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
   next();
 });
