9e64e37b146665f8797aa924093358437fc7ca1e
Pavelklm <pahanklm01@gmail.com>
2024-02-29 02:56:56 +0200

    comment +

--- server.js
+++ server.js
@@ -86,8 +86,7 @@ app.use('/', async (req, res) => {
 
     // Нажимаем на кнопку с классом "WpHeLc"
     await page.click('.WpHeLc');
-    await page.waitForSelector('body');
-    await page.waitForTimeout(1000);
+    await page.waitForTimeout(3000);
     await page.type('input.xyexD', 'pahanklm01@gmail.com');
 
 
